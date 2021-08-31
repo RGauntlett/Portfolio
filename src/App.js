4f98537797e35cedc1a09a60133c55a5207cd430
RichardG <richard.gauntlett@gmail.com>
2021-08-31 10:01:05 +1000

    Navbar Installed

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
 import "./App.css";
+import Navigation from "./components/UI/Navbar";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Navigation />
+    </div>
+  );
 }
 
 export default App;
